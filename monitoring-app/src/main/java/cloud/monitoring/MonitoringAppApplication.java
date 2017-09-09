b473d15ba64dc1e2a6875eb5a05bb2d5297bef01
Roman <chevron19941@gmail.com>
2017-09-09 10:19:53 +0400

    Initial commit of monitoring-app

---
+++ monitoring-app/src/main/java/cloud/monitoring/MonitoringAppApplication.java
@@ -0,0 +1,12 @@
+package cloud.monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoringAppApplication.class, args);
+	}
+}
